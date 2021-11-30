a4e21daf824dacfbc3df76ff967d46f3726d404f
Yagnesh Devashrayee <aim01learn@gmail.com>
2021-11-30 11:05:47 +0530

    hoisting example

---
+++ hoisting/hoistingExample.js
@@ -0,0 +1,23 @@
+//Variables declared or initialized will have their 
+//declaration "moved" up to the top of their module/function-level scope, which we refer to as hoisting
+console.log(varExample); //Prints undefined
+var varExample = 1;
+console.log(varExample); //Prints 1
+
+
+
+//function Declaration
+console.log(foo); // [Function : foo]
+foo(); // 'FOOOOO'
+function foo(){
+    console.log('FOOOOO');
+}
+console.log(foo); // [Function : foo]
+
+//Function Expression
+console.log(bar); //undefined
+bar(); // Uncaught TypeError: bar is not a function
+var bar = function (){
+    console.log('BARRRR');
+}
+console.log(bar); //[Function: bar]
